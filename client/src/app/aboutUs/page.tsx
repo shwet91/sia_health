807bd7ab92e8a53047e9ef78285a0c79be5136c2
Shwet Prakash <shwetprakash22@gmail.com>
2025-12-05 11:20:32 +0530

    Applied About Us v1 again to fix n8n url bug

--- client/src/app/aboutUs/page.tsx
+++ client/src/app/aboutUs/page.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
-// import AboutUs from '@/components/otherPages/AboutUs'
+import AboutUs from '@/components/otherPages/AboutUs'
 import Navbar from '@/components/landingPage/Navbar'
 import Footer from '@/components/landingPage/Footer'
-import AboutUs2 from '@/components/otherPages/AboutUs2'
+// import AboutUs2 from '@/components/otherPages/AboutUs2'
 
 function page() {
   return (
     <div>
         <Navbar />
-        {/* <AboutUs /> */}
-        <AboutUs2 />
+        <AboutUs />
+        {/* <AboutUs2 /> */}
         <Footer />
     </div>
   )
